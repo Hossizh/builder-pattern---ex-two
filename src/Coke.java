e6498d1cf4d00610f29b27ff4efbd46017ed025f
Hossein zharfi <hossein.zharfi74@gmail.com>
2024-11-25 17:09:24 +0330

    this is second example of Builder pattern

---
+++ src/Coke.java
@@ -0,0 +1,11 @@
+public class Coke extends ColdDrinks{
+    @Override
+    public String name() {
+        return "Coke";
+    }
+
+    @Override
+    public float price() {
+        return 10.5f;
+    }
+}
---
+++ src/Pepsi.java
@@ -0,0 +1,11 @@
+public class Pepsi extends ColdDrinks{
+    @Override
+    public String name() {
+        return "Pepsi";
+    }
+
+    @Override
+    public float price() {
+        return 13.5f;
+    }
+}
